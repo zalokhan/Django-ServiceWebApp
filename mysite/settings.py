ed6ab4c3f141fb8382d92d4b183491a86bdc8757
Zeeshan Lokhandwala <zlokhandwala@zlokhandwala-m01.vmware.com>
2016-04-05 01:01:56 -0700

    Changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,7 +123,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_URL = 'mysite/static/'
+STATIC_URL = 'app/mysite/static/'
 
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 
