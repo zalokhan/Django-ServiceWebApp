57711d542584d5b9ec3ba6a2d1208781e55c1df5
Zeeshan Lokhandwala <zlokhandwala@zlokhandwala-m01.vmware.com>
2016-04-05 01:07:20 -0700

    Changed settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -125,8 +125,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 
 STATICFILES_DIRS = [
-    os.path.join(PROJECT_ROOT, 'static')
+    "/app/mysite/staticfiles/service"
 ]
