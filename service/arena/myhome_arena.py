8355cb4aba60a130790bba60843934f1a31eb6f3
Zeeshan Lokhandwala <zlokhandwala@zlokhandwala-m01.vmware.com>
2016-04-10 04:04:27 -0700

    Changed UI completely

--- service/arena/myhome_arena.py
+++ service/arena/myhome_arena.py
@@ -1,6 +1,7 @@
 from django.contrib.auth import logout
 from django.shortcuts import render
 
+
 def my_home_main_arena(request):
     logout(request)
     return render(request, 'service/user_home.html')
